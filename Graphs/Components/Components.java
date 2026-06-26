30c80299d5c490ba7e38be499aad75d01dabfaf9
AkshitaSharma211 <sharma.1608akshita@gmail.com>
2026-06-26 14:52:58 +0530

    Add largest connected component using DFS

--- Graphs/Components/Components.java
+++ Graphs/Components/Components.java
@@ -40,25 +40,47 @@ void addEdge(int u,int v){
         return count;
     }
 
-void dfs(int node, boolean[] visited){
+int dfs(int node, boolean[] visited){
+    int count=1;
     visited[node] = true;
     System.out.print(node + "  ");
 
     for(int neighbor: adj.get(node)){
         if(!visited[neighbor]){
-            dfs(neighbor, visited);
+          count+= dfs(neighbor, visited);
+
         }
     }
+    return count;
 }
 
+int largestComponent(){
+    int largest=0;
+    boolean visited[] = new boolean[Vertices];
+    int count=1;
+    for(int i=0;i<Vertices;i++){
+        if(!visited[i]){
+            count = dfs(i, visited);
+                largest= Math.max(largest,count);
+
+        }
+    }
+    
+    return largest;
+}
 
 public static void main(String[] args){
-
-     Components g = new Components(5);
+Components g = new Components(7);
 g.addEdge(0, 1);
-g.addEdge(2, 3);
-// node 4 is alone
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(4, 5);
+g.addEdge(4, 6);
+g.addEdge(5, 6);
+
 System.out.println("Components: " + g.countComponents());
+System.out.println("Largest Component: " + g.largestComponent());
+
 }
 }
     
